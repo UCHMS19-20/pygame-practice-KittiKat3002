50f6522e2efd52fcd622c05ac8d4cd4d23e876f0
KittiKat3002 <58003950+KittiKat3002@users.noreply.github.com>
2019-12-12 13:23:13 -0500

    made text move?

--- text.py
+++ text.py
@@ -4,10 +4,14 @@ import pygame
 # Initialize pygame so it runs in the background and manages things
 pygame.init()
 
-purble = pygame.Color(100, 225, 225)
+miku = pygame.Color(100, 225, 225)
+red = pygame.Color(100, 0, 0)
 
 # Create a display. Size must be a tuple, which is why it's in parentheses
 screen = pygame.display.set_mode( (400, 400) )
+font = pygame.font.Sysfont("Comic Sans",  100)
+
+text = font.render("blood", True, red)
 
 # Main loop. Your game would go inside this loop
 while True:
@@ -18,5 +22,6 @@ while True:
         if event.type == pygame.QUIT:
             # If so, exit the program
             sys.exit()
-    screen.fill(purble)
+    screen.fill(miku)
+    screen.blit(text, pygame.mouse.get_pos())
     pygame.display.flip()
